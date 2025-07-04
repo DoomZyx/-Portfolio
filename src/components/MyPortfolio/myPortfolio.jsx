0fb1622be567f42fc55a379373e6a2aa6c5f7ca0
DoomZyx <axelcella.ac@gmail.com>
2025-07-04 20:16:25 +0200

    feat(myportfolio) : switched grid to flexbox

--- src/components/MyPortfolio/myPortfolio.jsx
+++ src/components/MyPortfolio/myPortfolio.jsx
@@ -42,57 +42,61 @@ function MyPortfolio() {
     <>
       <h2 className="title-portfolio">Mon portfolio</h2>
 
-      <div className="layoutgrid">
-        <div className="kasa">
-          <img src={kasa} alt="" />
-          <div className="info-project">
-            <h4>Kasa</h4>
-            <FontAwesomeIcon icon={faArrowRight} />
+      <div className="container">
+        <div className="containerThirst">
+          <div className="kasa">
+            <img src={kasa} alt="" />
+            <div className="info-project">
+              <h4>Kasa</h4>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+          </div>
+
+          <div className="leMas">
+            <img src={LeMas} alt="" />
+            <div className="info-project">
+              <h4>Le Mas Du Paradis Bleu</h4>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+          </div>
+
+          <div className="Iphone3d" ref={containerRef}>
+            <Spline
+              className="iphone"
+              scene="https://prod.spline.design/ddMWGaWGSLLGgFdz/scene.splinecode"
+            />
           </div>
         </div>
 
-        <div className="leMas">
-          <img src={LeMas} alt="" />
-          <div className="info-project">
-            <h4>Le Mas Du Paradis Bleu</h4>
-            <FontAwesomeIcon icon={faArrowRight} />
+        <div className="secondlayout">
+          <div className="space">
+            <img src={space} alt="" />
           </div>
-        </div>
 
-        <div className="Iphone3d" ref={containerRef}>
-          <Spline
-            className="iphone"
-            scene="https://prod.spline.design/ddMWGaWGSLLGgFdz/scene.splinecode"
-          />
-        </div>
-
-        <div className="space">
-          <img src={space} alt="" />
-        </div>
-
-        <div className="layout-stack">
-          <div className="thirstrangest">
-          <img className="nodejs" src={node} alt="" />
-          <img className="sql" src={sql} alt="" />
-          <img className="mongodb" src={mongodb} alt="" />
+          <div className="layout-stack">
+            <div className="thirstrangest">
+              <img className="nodejs" src={node} alt="" />
+              <img className="sql" src={sql} alt="" />
+              <img className="mongodb" src={mongodb} alt="" />
+            </div>
+            <div className="secondrangest">
+              <img className="react" src={react} alt="" />
+              <img className="sass" src={sass} alt="" />
+              <img className="figma" src={figma} alt="" />
+            </div>
           </div>
-          <div className="secondrangest">
-          <img className="react" src={react} alt="" />
-          <img className="sass" src={sass} alt="" />
-          <img className="figma" src={figma} alt="" />
-          </div>
-        </div>
 
-        <div className="ohmyfood">
-          <img src={ohmyfood} alt="" />
-          <div className="info-project">
-            <h4>Ohmyfood</h4>
-            <FontAwesomeIcon icon={faArrowRight} />
+          <div className="ohmyfood">
+            <img src={ohmyfood} alt="" />
+            <div className="info-project">
+              <h4>Ohmyfood</h4>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
           </div>
-        </div>
 
-        <div className="myself">
-          <img src={character} alt="" />
+          <div className="myself">
+            <img src={character} alt="" />
+          </div>
         </div>
       </div>
     </>
